e4c1a8b8ef213d1e05d6e542b7dd78d7f144d00c
samyamer <SAMYAMER2000@GMAIL.COM>
2024-04-24 15:20:10 -0400

    classification

--- analysis.py
+++ analysis.py
@@ -1,33 +1,17 @@
 import numpy as np
 import csv
 from sklearn.model_selection import train_test_split
-import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, Dense
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.svm import SVC
 
 sites = {"AMZN":0,"BKNG":1, "CHESS":2, "DDG":3, "GIT":4, "GOGL":5, "TKTK":6, "WKPDA":7,"X":8,"YT":9}
 
 
-# with open("./AMZN/0.txt", 'r') as read_obj: 
-#         csv_reader = csv.reader(read_obj) 
-#         list_of_csv = list(csv_reader) 
-        
-#         arr = np.array(list_of_csv)
-#         arr = np.asarray(arr,dtype="int")
-#         X[0] = arr
-
-# print(len(list_of_csv))
-
-# print(type(list_of_csv[0][0]))
-
-# print(len(arr))
-# print(arr[0])
-# print(type(arr[0][0]))
-# print(arr.shape)
-
-X = np.zeros((110,25000,15))
+X = np.zeros((110,375000))
 x_ndx = 0
-Y = np.array((110))
+Y = np.zeros((110))
+
 
 for site in sites.keys():
     for i in range(11):
@@ -39,6 +23,7 @@ for site in sites.keys():
             
             arr = np.array(list_of_csv)
             arr = np.asarray(arr,dtype="int")
+            arr = np.reshape(arr,375000)
             X[x_ndx] = np.copy(arr)
             Y[x_ndx] = sites[site]
             x_ndx+=1
@@ -49,3 +34,29 @@ print(X_train.shape)
 print(X_test.shape)
 print(Y_train.shape)
 print(Y_test.shape)
+
+
+rf = RandomForestClassifier(n_estimators=100, random_state=42) 
+
+
+rf.fit(X_train, Y_train)
+
+y_pred = rf.predict(X_test)
+
+
+print("Accuracy:", accuracy_score(Y_test, y_pred))
+print("\nClassification Report:\n", classification_report(Y_test, y_pred))
+
+print("--------------SVM-----------------")
+
+svm = SVC(kernel='linear', random_state=42)  
+svm.fit(X_train, Y_train)
+
+
+y_pred = svm.predict(X_test)
+
+
+print("Accuracy:", accuracy_score(Y_test, y_pred))
+print("Classification Report:", classification_report(Y_test, y_pred))
+
+
